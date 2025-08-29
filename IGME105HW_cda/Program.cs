5a3712b4f02aeda209583327cc577975eebe39b7
cda7733 <cda7733@rit.edu>
2025-08-29 13:40:15 -0400

    trying change
    changed "Hello, World!" to "hello, world!"

---
+++ IGME105HW_cda/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace IGME105HW_cda;
+
+// program name: IGME105 monopoly game
+// created by: charisma allen
+// purpose: make a monopoly game but w/ gambling
+/* modifications: 
+ * 8/27/2025 = started configuring program - cda
+ * 8/29/2025 = changed "Hello, World!" to "hello, world!"
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello, world!");
+    }
+}
